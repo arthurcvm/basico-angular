8c613a78eb92184146809f5186f2a0ceb25ee687
Arthur Cezar <arthurcvm@hotmail.com>
2024-05-09 23:23:01 -0300

    templates e interpolação

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,26 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello World</h1>
-    `
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent {
 
+    nome : string;
+    clientes: Cliente[];
+
+    constructor(){
+        this.nome = 'Arthur';
+        let fulano = new Cliente('Fulano', 30);
+        let cicrano = new Cliente('Cicrano', 25);
+        let outro = new Cliente('Outro', 33);
+
+        this.clientes = [fulano, cicrano, outro];
+    }
+}
+
+class Cliente{
+    constructor(
+        public nome: string, 
+        public idade: number
+    ){}
 }
